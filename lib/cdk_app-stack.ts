43babf3fbbefa3e63385036d9ca52b1db20ccff3
Aliaksei Kavalko <Aliaksei.Kavalko@itechart-group.com>
2022-02-08 10:10:23 +0300

    initial commit

--- lib/cdk_app-stack.ts
+++ lib/cdk_app-stack.ts
@@ -1,5 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkAppStack extends Stack {
@@ -12,5 +14,11 @@ export class CdkAppStack extends Stack {
     // const queue = new sqs.Queue(this, 'CdkAppQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
   }
 }
